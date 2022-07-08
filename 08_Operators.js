372decef1b03d8f0367392ff03c4ba31dc7e3a6b
Nayan Patel <nayanpatel00112@gmail.com>
2022-07-08 14:43:44 +0530

    JavaScript

--- 08_Operators.js
+++ 08_Operators.js
@@ -31,6 +31,8 @@ console.log(a--); // 4 ('a' was 4)
 console.log(--b); // 5 ('b' was 6)
 
 
+//---------------------------------------------------------------------------
+
 // 2. Comparison (Relational) Operators
 
 var x = 4; // int
@@ -51,6 +53,84 @@ console.log(x === w);  //false
 console.log(x !== w);  // true
 
 
+//---------------------------------------------------------------------------
+
 // 3. Bitwise Operators:
 
+console.log((10 == 20) & (10 == 10)); // 0 (false)
+console.log((10 == 20) | (10 == 10)); // 1 (true)
+console.log((10 == 20) ^ (10 == 10)); // 1 (true)
+console.log(~(10 === '10')); //-1 true
+console.log(~(10 == '10')); //-2 true
+console.log(10 << 2);  // 40
+console.log(10 >> 2);  // 2
+console.log(10 >>> 2); // 2
 
+
+//---------------------------------------------------------------------------
+
+// 4. Logical Operators:
+
+console.log((3 > 4) && (4 != 3)); //false
+console.log((3 > 4) || (4 != 3)); //false
+console.log(!(10 == '10')); //false
+console.log(!(2 != 2)); //true
+
+
+//---------------------------------------------------------------------------
+
+// 5. Assignment Operators
+
+var p;
+p = 4;
+console.log(p);   // 4
+p += 2
+console.log(p);   // 6
+p -= 1;
+console.log(p);   // 5
+p *= 4;
+console.log(p);   // 20
+p /= 2;
+console.log(p);   // 10
+p %= 2;
+console.log(p);   // 0
+
+
+//---------------------------------------------------------------------------
+
+
+// JavaScript Special Operators:
+/*
+The following operators are known as JavaScript special operators.
+
+    (?:)
+        Conditional Operator returns value based on the condition. It is like if-else.
+
+    ,
+        Comma Operator allows multiple expressions to be evaluated as single statement.
+
+    delete
+        Delete Operator deletes a property from the object.
+
+    in
+        In Operator checks if object has the given property
+
+    instanceof
+        checks if the object is an instance of given type
+
+    new
+        creates an instance (object)
+
+    typeof
+        checks the type of object.
+
+    void
+        it discards the expression's return value.
+
+    yield
+        checks what is returned in a generator by the generator's iterator.
+
+    Next
+        TopicJavaScript If-else Statement
+
+*/ 
