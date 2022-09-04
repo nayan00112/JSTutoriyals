45b24f8b67fa1289e407a9d309f5e22f683b4fcd
Nayan Patel <nayanpatel00112@gmail.com>
2022-09-04 21:39:35 +0530

    JS

---
+++ Pratctice/testTHIS.js
@@ -0,0 +1,32 @@
+
+
+// // console.log(this);
+
+// var TEST = ()=>{
+// // console.log(this);
+
+// var n = "n";
+// console.log("Nayan");
+// var test2 = ()=>{
+//     var n = 6;
+//     console.log(this);
+// }
+// test2();
+// console.log(this);
+
+
+// }
+// // console.log(this);
+
+// TEST();
+// // console.log(this);
+
+
+var nayan = {
+    name: "Nayan",
+    serName: "Patel",
+
+    // console.log(this);
+}
+console.log(this);
+console.log(nayan.name);
